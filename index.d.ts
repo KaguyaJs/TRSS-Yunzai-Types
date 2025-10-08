0a7c8d5dbe12737a69f29f07ff9a187cba271120
等风来 <dengfenglai547@qq.com>
2025-10-08 22:28:25 +0800

    🚀

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,6 @@
 import type { Redis } from "./redis.d.ts"
 import type { Yunzai, Client } from "./Bot.d.ts"
 import type { Logger } from "./logger.d.ts"
-// import type { Plugin } from "./plugin.d.ts"
 import type { Utils } from "./util.d.ts"
 import type { Segment } from "./segment.d.ts"
 
@@ -13,11 +12,10 @@ declare global {
   var redis: Redis
   var segment: Segment
   var logger: Logger
-  // var plugin: typeof Plugin
 }
 
 export type * from "./Bot.d.ts"
 export type * from "./logger.d.ts"
 export type * from "./plugin.d.ts"
 export type * from "./util.d.ts"
-// export type * from "icqq"
+export type * as icqq from "icqq"
